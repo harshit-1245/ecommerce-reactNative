16d5ad4a98648d81d693e84129b4d2af34972c01
harshit-506 <supriyamohapatra658@gmail.com>
2023-12-11 17:45:00 +0530

    work in prgress

--- ecommerceApp/navigation/StackNavigator.js
+++ ecommerceApp/navigation/StackNavigator.js
@@ -1,17 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import LoginScreen from '../screens/LoginScreen'
 import RegisterScreen from "../screens/RegisterScreen"
-
+//--------------------> more like react routing
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
   return (
    <NavigationContainer>
     <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{headerShown:false}} />
     </Stack.Navigator>
    </NavigationContainer>
   )
--- ecommerceApp/screens/LoginScreen.js
+++ ecommerceApp/screens/LoginScreen.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
+
 import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView style={{flex:1 ,backgroundColor:"white",alignItems:"center"}}>
+      <View>
+        
+        <Image
+        style={{width:150,height:150}}
+        source={{
+          uri: '../assets/ecommerceImages/amazon.png',
+        }}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
